d384c89751df35533e455784f8d2e873ed220c29
Matheusdnf <matheusdiniz870@gmail.com>
2025-03-31 11:32:57 -0300

    feat - app vue

---
+++ task_4/matheus_diniz/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Dados da Operadora de Saúde</h1>
+    <operator-data />
+  </div>
+</template>
+
+<script>
+import OperatorData from "./components/OperatorData.vue";
+
+export default {
+  name: "App",
+  components: {
+    OperatorData,
+  },
+};
+</script>
+
+<style>
+/* Estilos globais podem ficar aqui */
+</style>
