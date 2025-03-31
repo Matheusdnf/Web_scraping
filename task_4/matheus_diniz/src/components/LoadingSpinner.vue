166f259d0dc37c261edf0096fea3a66d064eb22e
Matheusdnf <matheusdiniz870@gmail.com>
2025-03-31 11:33:26 -0300

    feat - carregamento da página

---
+++ task_4/matheus_diniz/src/components/LoadingSpinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loading-spinner">Carregando...</div>
+</template>
+
+<script>
+export default {
+  name: "LoadingSpinner",
+};
+</script>
+
+<style scoped>
+.loading-spinner {
+  margin: 20px 0;
+  color: #4caf50;
+  font-weight: bold;
+}
+</style>
