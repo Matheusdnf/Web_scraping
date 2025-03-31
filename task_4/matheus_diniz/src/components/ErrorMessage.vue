db2a225008e4fec51d261ed63ed99070306b01f0
Matheusdnf <matheusdiniz870@gmail.com>
2025-03-31 11:31:45 -0300

    feat - mensagem de erro

---
+++ task_4/matheus_diniz/src/components/ErrorMessage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error-message">Ocorreu um erro: {{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "ErrorMessage",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.error-message {
+  color: red;
+  margin: 10px 0;
+  font-weight: bold;
+}
+</style>
